cedd663a32c6e3a432045016d2c8127d21d43b44
matheusgonzalez91 <gonzalezmatheus01@gmail.com>
2024-04-22 19:52:44 -0300

    Implementando páginas e adicionando imagens

--- src/app/pages/java/java.component.ts
+++ src/app/pages/java/java.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-java',
   templateUrl: './java.component.html',
   styleUrls: ['./java.component.scss']
 })
-export class JavaComponent {
+export class JavaComponent implements OnInit{
 
+  ngOnInit(): void {
+    window.scrollTo(0, 0);
+  }
 }
--- src/app/pages/pythonfordevs/pythonfordevs.component.ts
+++ src/app/pages/pythonfordevs/pythonfordevs.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pythonfordevs',
   templateUrl: './pythonfordevs.component.html',
   styleUrls: ['./pythonfordevs.component.scss']
 })
-export class PythonfordevsComponent {
-
+export class PythonfordevsComponent implements OnInit{
+  ngOnInit(): void {
+    window.scrollTo(0, 0);
+  }
 }
